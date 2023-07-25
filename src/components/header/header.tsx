193351a624d527c405ab663ddd81b808099e5c69
IzzyCap <isicaparros@gmail.com>
2023-07-25 23:05:23 +0200

    feat: add mobile toolbar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './header.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Header() {
-  return (
+  const [showNavbar, setShowNavbar] = useState(false);
 
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
+  return (
     <nav>
-      <ul className={classes.primaryNav}>
+      <div className={classes.menuIcon} onClick={handleShowNavbar}>
+        <img alt='burger button' src='/icons/burger.svg'></img>
+      </div>
+      <ul className={`${classes.primaryNav} ${showNavbar && classes.active}`}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
@@ -16,17 +24,18 @@ export default function Header() {
           <Image
             src="/logo.svg"
             alt="Izzy Logo"
-            className={classes.izzyLogo}
+            className={classes.logo}
             width={200}
             height={48}
             priority
           />
         </a>
-        <li><Link href='/'>Projects</Link></li>
-        <li><Link href='/assets'>Assets</Link></li>
-        <li className={classes.left}><Link href='/info'>Info</Link></li>
-        <li><a href="#">Donate</a></li>
-        <li><a href="#">Support</a></li>
+        <li className={classes.primaryLi}><Link className={classes.link} href='/'>Projects</Link></li>
+        <li className={classes.primaryLi}><Link className={classes.link} href='/assets'>Assets</Link></li>
+        <li className={`${classes.left} ${classes.primaryLi}`}><Link className={classes.link} href='/info'>Info</Link></li>
+        <hr/>
+        <li className={classes.secondaryLi}><Link href="#">Donate</Link></li>
+        <li className={classes.secondaryLi}><Link  href="#">Support</Link></li>
       </ul>
     </nav> 
   )
